4773151b8c80eeebbaa6d16d1b43077050ad4c8d
Jeya112 <73278772+Jeya112@users.noreply.github.com>
2020-10-30 10:55:43 +0530

    Update Url.java

--- Lab3Assignments/src/Url.java
+++ Lab3Assignments/src/Url.java
@@ -9,21 +9,16 @@ import java.util.*;
 public class Url{
 
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
+                Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the Url");
 		String url = sc.nextLine();
 		System.out.println("The Command in the Url is "+getCommand(url));
 	}
-
-
-	private static String getCommand(String url) {
-		
-		
-		String[] str1 = url.split("[/]");     
+private static String getCommand(String url) {
+		String[] str1 = url.split("[/]");          //splitting 
 		int length=str1.length;
-		String temp= str1[length-1];     
-		String str2[]=temp.split("[.]");
+		String temp= str1[length-1];               //taking last string
+		String str2[]=temp.split("[.]");           //splitting
 		String command =(str2[0]);;
 	    return command;    
 	}
